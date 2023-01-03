e6053d9bb77136c37d6b35ac6add7fec59dad205
UniDevs <114398836+UniDevsIT@users.noreply.github.com>
2023-01-03 18:22:25 +0600

    committinggg

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
         //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
 
-        // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
